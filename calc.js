10730cf905a8ec5f28930fb8033714a578ecb37f
Neal Bayya <neal.bayya@gmail.com>
2020-05-25 17:25:45 -0400

    Created Responsive Display (HTML+CSS)

---
+++ calc.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let expr = [];
+let b0 = document.querySelector('#zero')
+let b1 = document.querySelector('#one')
+let b2 = document.querySelector('#two')
+let b3 = document.querySelector('#three')
+let b4 = document.querySelector('#four')
+let b5 = document.querySelector('#five')
+let b6 = document.querySelector('#six')
+let b7 = document.querySelector('#seven')
+let b8 = document.querySelector('#eight')
+let b9 = document.querySelector('#nine')
+let eval = document.querySelector('#return')
+let plus = document.querySelector('#plus')
+let minus = document.querySelector('#minus')
+let multiply = document.querySelector('#multiply')
+let divide = document.querySelector('#divide')
+
